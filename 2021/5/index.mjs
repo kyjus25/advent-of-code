c084e4b73c58007784335cedc657fa767e561558
Justin White <kyjus25@gmail.com>
2021-12-05 16:18:57 -0600

    Should be working, just need to get map dimensions

--- 2021/5/index.mjs
+++ 2021/5/index.mjs
@@ -8,7 +8,7 @@ const input  = inputFile
                 .map(i => {
                     const coords1 = i[0].split(',');
                     const coords2 = i[1].split(',');
-                    return {x1: coords1[0], y1: coords1[1], x2: coords2[0], y2: coords2[1] }
+                    return {x1: parseInt(coords1[0]), y1: parseInt(coords1[1]), x2: parseInt(coords2[0]), y2: parseInt(coords2[1]) }
                 });
 
 const MAP = [];
@@ -24,15 +24,37 @@ const createMap = () => {
 }
 
 const plot = (coords) => {
-    addPoint(coords.x1, coords.y1);
-    addPoint(coords.x2, coords.y2);
-    if (coords.x1 === coords.x2) { 
-        console.log('vertical')
-    } else {
-        console.log('horizontal')
+    let method = 'NONE';
+    let start = -1;
+    let end = -1;
+    let pivot = -1;
+    if (coords.x1 === coords.x2) {
+        method = 'VERTICAL';
+        pivot = coords.x1;
+        if (coords.y1 < coords.y2) { start = coords.y1; end = coords.y2; } else { start = coords.y2; end = coords.y1; }
+    } else if (coords.y1 === coords.y2) {
+        method = 'HORIZONTAL';
+        pivot = coords.y1;
+        if (coords.x1 < coords.x2) { start = coords.x1; end = coords.x2; } else { start = coords.x2; end = coords.x1; }
     }
-    // point = point = '.' ? 1 : point + 1;
-    // console.log('POINT', MAP[coords.y1][coords.x1]);
+    if (method === 'NONE') { return; }
+    addPoints(method, pivot, start, end);
+}
+
+const addPoints = (method, pivot, start, end) => {
+    switch (method) {
+        case 'VERTICAL':
+            for (let i = start; i <= end; i++) {
+                addPoint(pivot, i);
+            }
+            return;
+        case 'HORIZONTAL':
+            for (let i = start; i <= end; i++) {
+                addPoint(i, pivot);
+            }
+            return;
+    }
+    
 }
 
 const addPoint = (x,y) => {
@@ -50,8 +72,5 @@ input.forEach(coords => {
     plot(coords);
 });
 log();
-
-
-
 
 // console.log('input', input);
