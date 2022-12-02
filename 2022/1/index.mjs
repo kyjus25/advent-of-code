75a9fc896cb8b4c30696a84ade848f1d45bd7465
Justin White <kyjus25@gmail.com>
2022-12-02 00:19:09 -0600

    Include comments

--- 2022/1/index.mjs
+++ 2022/1/index.mjs
@@ -5,13 +5,20 @@ const input = inputFile.split('\n').map(i => parseInt(i) || null);
 
 let sums = [];
 let index = 0;
+
 input.forEach(i => {
+    // Each elf is separated by a null value, use this to bucket the calories
     if (!i) { index++; return }
+    // If the index hasn't been created yet, set it to zero
     if (!sums[index]) { sums[index] = 0; }
+    // Create a running total of the index as we traverse the file
     sums[index] = sums[index] + i;
 });
+// Sort the buckets largest to smallest
 sums = sums.sort((a,b) => a < b ? 1 : -1);
 
-console.log(sums);
+// The calories of the top elf
 console.log('most', sums[0]);
-console.log('total 3', sums.slice(0, 3).reduce((a,b) => a + b))
+
+// The sum of the top 3 elves
+console.log('total 3', sums.slice(0, 3).reduce((a,b) => a + b));
