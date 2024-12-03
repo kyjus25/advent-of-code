87222e33267a3a195dad17d59dae5b086c0a5655
Justin White <kyjus25@gmail.com>
2024-12-02 19:53:09 -0600

    Quicksave

--- 2024/2/1.ts
+++ 2024/2/1.ts
@@ -1,46 +1,44 @@
 const file = await Bun.file("input.txt").text();
 // Split the file by rows
-const rows = file.split("\n").map((i) => i.split(" ").map(i => parseInt(i)));
+const rows = file.split("\n").map((i) => i.split(" ").map((i) => parseInt(i)));
 // console.log(rows);
 
 const checks: boolean[] = rows.reduce((acc, curr) => {
+  // Get the differences between each adjacent number
+  const differences: Array<number> = curr
+    .map((i, index) => {
+      const current = i;
+      const next = curr[index + 1];
+      if (!next) return null;
+      return current - next;
+    })
+    .filter((i) => i !== null);
 
-    // Get the differences between each adjacent number
-    const differences: Array<number> = curr.map((i, index) => {
-        const current = i;
-        const next = curr[index + 1];
-        if (!next) return null;
-        return current - next;
-    }).filter(i => i !== null);
+  // Calculate all pos and all neg for future calculations
+  const pos: number[] = differences.map((i) => Math.abs(i));
+  const neg: number[] = differences.map((i) => Math.abs(i) * -1);
 
-    // Calculate all pos and all neg for future calculations
-    const pos: number[] = differences.map(i => Math.abs(i));
-    const neg: number[] = differences.map(i => Math.abs(i) * -1);
-
-    // Flag if either not all positive or not all negative
-    if (
-        JSON.stringify(differences) !== JSON.stringify(pos) && 
-        JSON.stringify(differences) !== JSON.stringify(neg)
-    ) {
-        // console.log('differences', differences, false);
-        acc.push(false);
-        return acc;
-    }
-
-    // Flag if any difference is less than 1 or greater than 3
-    if (
-        pos.find(i => i === 0) !== undefined ||
-        pos.find(i => i > 3)
-    ) {
-        // console.log('differences', differences, false);
-        acc.push(false);
-        return acc;
-    }
-    
-    // console.log('differences', differences, true);
-    acc.push(true);
+  // Flag if either not all positive or not all negative
+  if (
+    JSON.stringify(differences) !== JSON.stringify(pos) &&
+    JSON.stringify(differences) !== JSON.stringify(neg)
+  ) {
+    // console.log('differences', differences, false);
+    acc.push(false);
     return acc;
+  }
+
+  // Flag if any difference is less than 1 or greater than 3
+  if (pos.find((i) => i === 0) !== undefined || pos.find((i) => i > 3)) {
+    // console.log('differences', differences, false);
+    acc.push(false);
+    return acc;
+  }
+
+  // console.log('differences', differences, true);
+  acc.push(true);
+  return acc;
 }, [] as boolean[]);
 
 // The answer is how ever many are safe levels
-console.log(checks.filter(i => !!i).length);
+console.log(checks.filter((i) => !!i).length);
--- 2024/2/2.ts
+++ 2024/2/2.ts
@@ -1,53 +1,43 @@
 const file = await Bun.file("input.txt").text();
 // Split the file by rows
-const rows = file.split("\n").map((i) => i.split(" ").map(i => parseInt(i)));
+const rows = file.split("\n").map((i) => i.split(" ").map((i) => parseInt(i)));
 // console.log(rows);
 
+const getLevels = (row: number[]) => {
+  // Get the differences between each adjacent number
+  const differences: Array<number> = row
+    .map((i, index) => {
+      const current = i;
+      const next = row[index + 1];
+      if (!next) return null;
+      return current - next;
+    })
+    .filter((i) => i !== null);
+  // Calculate all pos and all neg for future calculations
+  const pos: number[] = differences.map((i) => Math.abs(i));
+  const neg: number[] = differences.map((i) => Math.abs(i) * -1);
+  // Flag if either not all positive or not all negative
+  if (
+    JSON.stringify(differences) !== JSON.stringify(pos) &&
+    JSON.stringify(differences) !== JSON.stringify(neg)
+  ) {
+    return false;
+  }
+  // Flag if any difference is less than 1 or greater than 3
+  if (pos.find((i) => i === 0) !== undefined || pos.find((i) => i > 3)) {
+    // console.log('differences', differences, false);
+    return false;
+  }
+  return true;
+};
+
 const checks: boolean[] = rows.reduce((acc, curr) => {
+  const levels = getLevels(curr);
 
-    // Get the differences between each adjacent number
-    const differences: Array<number> = curr.map((i, index) => {
-        const current = i;
-        const next = curr[index + 1];
-        if (!next) return null;
-        return current - next;
-    }).filter(i => i !== null);
-
-    // Calculate all pos and all neg for future calculations
-    const pos: number[] = differences.map(i => Math.abs(i));
-    const neg: number[] = differences.map(i => Math.abs(i) * -1);
-
-    // Count the number of unsafe levels
-    let unsafe: number = 0;
-
-    // Flag if either not all positive or not all negative
-    if (
-        JSON.stringify(differences) !== JSON.stringify(pos) && 
-        JSON.stringify(differences) !== JSON.stringify(neg)
-    ) {
-        // console.log('differences', differences, false);
-        unsafe += 1;
-        // return acc;
-    }
-
-    // Flag if any difference is less than 1 or greater than 3
-    if (
-        pos.find(i => i === 0) !== undefined ||
-        pos.find(i => i > 3)
-    ) {
-        // console.log('differences', differences, false);
-        unsafe += 1;
-        // return acc;
-    }
-    
-    // console.log('differences', differences, true);
-    // acc.push(true);
-
-    
-    acc.push(unsafe);
-    return acc;
-}, [] as number[]);
+  console.log(levels);
+  acc.push(levels);
+  return acc;
+}, [] as boolean[]);
 
 // The answer is how ever many are safe levels
-// console.log(checks.filter(i => !!i).length);
-console.log(checks)
+console.log(checks.filter((i) => !!i).length);
